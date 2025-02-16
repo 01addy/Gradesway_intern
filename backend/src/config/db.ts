54218f40deb23425e69a6ea0ec5aa90f8aafeaf5
Aditya Pratap Singh <132531853+01addy@users.noreply.github.com>
2025-02-16 22:04:59 +0530

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,5 +1,8 @@
+// Import PrismaClient from the Prisma package to interact with the database
 import { PrismaClient } from "@prisma/client";
 
+// Create a new instance of PrismaClient to manage database connections
 const prisma = new PrismaClient();
 
+// Export the Prisma instance so it can be used in other parts of the application
 export default prisma;
